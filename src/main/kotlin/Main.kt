5354fc440543278384c57a15836d0083084ab463
Adriana <adriana.redep.matosevic@gmail.com>
2023-07-20 14:03:21 +0200

    Druga verzija za doradu.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,21 @@
 /** Ponavljanje svih lekacija */
 
-/*fun main(args: Array<String>) {
+/*fun main() {
     // 1. Mobile notification
-    fun main() {
-        val morningNotification = 51
-        val eveningNotification = 135
+    val morningNotification = 51
+    val eveningNotification = 135
 
-        printNotificationSummary(morningNotification)
-        printNotificationSummary(eveningNotification)
-    }
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
 
-
-    fun printNotificationSummary(numberOfMessages: Int) {
-        // Fill in the code.
-        if(numberOfMessages<100) println("You have $numberOfMessages notifications")
-    }
-// You have 51 notifications.
-//Your phone is blowing up! You have 99+ notifications.
+fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+    if (numberOfMessages < 100) println("You have $numberOfMessages notifications.")
+    else
+        println("Your phone is blowing up! You have 99+ notification.")
 }*/
+
 
 /*fun main() {
 //    A children's ticket price of $15 for people 12 years old or younger.
@@ -36,26 +34,28 @@
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    // Fill in the code.
-    return when (age) {
-        age in 0<=..<= 12 -> 15
-        age in 13 <=..<=16 ->
-        -> 15
+
+    return when {
+        age <= 12 && age >= 0 -> 15
+        age in 13..16 -> if (isMonday) 25 else 30
+        age >= 61 -> 20
+        else -> -1
     }
 }*/
 
+/** Stupnjevi */
+//There are three main temperature scales used in the world: Celsius, Fahrenheit, and Kelvin.
+//In the initial code provided in the following code snippet,
+//write a program that converts a temperature from one scale to another with these formulas:
+//
+//Celsius to Fahrenheit: ° F = 9/5 (° C) + 32
+//Kelvin to Celsius: ° C = K - 273.15
+//Fahrenheit to Kelvin: K = 5/9 (° F - 32) + 273.15
+//Note that the String.format("%.2f", /* measurement */ ) method is used to convert a number into a String type with 2 decimal places.
+//
+
 /*
-There are three main temperature scales used in the world: Celsius, Fahrenheit, and Kelvin.
-In the initial code provided in the following code snippet,
-write a program that converts a temperature from one scale to another with these formulas:
-
-Celsius to Fahrenheit: ° F = 9/5 (° C) + 32
-Kelvin to Celsius: ° C = K - 273.15
-Fahrenheit to Kelvin: K = 5/9 (° F - 32) + 273.15
-Note that the String.format("%.2f", /* measurement */ ) method is used to convert a number into a String type with 2 decimal places.
- */
-
-/*fun main() {
+fun main() {
     // Fill in the code.
     printFinalTemperature(27.0, "Celsius", "Fahrenheit", conversionFormula = { 9.0 / 5.0 * it + 32 })
     printFinalTemperature(350.0, "Celsius", "Fahrenheit", conversionFormula = { it - 273.15 })
@@ -71,19 +71,18 @@ fun printFinalTemperature(
 ) {
     val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
     println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
-}*/
+}
+*/
 
-/*
-5. Song catalog
-Imagine that you need to create a music-player app.
 
-Create a class that can represent the structure of a song. The Song class must include these code elements:
+/** 5. Song catalog */
+//Imagine that you need to create a music-player app.
+//Create a class that can represent the structure of a song. The Song class must include these code elements:
+//Properties for the title, artist, year published, and play count
+//A property that indicates whether the song is popular. If the play count is less than 1,000, consider it unpopular.
+//A method that prints a song description in this format:
+//"[Title], performed by [artist], was released in [year published]."
 
-Properties for the title, artist, year published, and play count
-A property that indicates whether the song is popular. If the play count is less than 1,000, consider it unpopular.
-A method that prints a song description in this format:
-"[Title], performed by [artist], was released in [year published]."
- */
 /*
 fun main() {
     val song = Song("Magdalena", "Oliver Dragojevic", 1978, 2000)
@@ -102,35 +101,40 @@ class Song(
     fun printDescription() {
         println("$title, performed by $artist, was realised in $yearPublished")
     }
-}*/
+}
+*/
 
 /** Kolekcije */
+
 fun main() {
 //    Liste
     val sentence = "I was at school"
 
-    val list: List<String> = listOf("matej", "ivan", "matej")
-//    println(listOf)
-    val listOfString = sentence.split(" ")
-    println(listOfString)
+//    val list = listOf<String>()
+    val list = listOf("matej", "ivan", "matej") // 00:21
+    println(list[1]) // ivan
 
-//    Setovi - rijetko se koriste
-    val randomSet = setOf("a", "a", "b") // izbacuje duplikate
-    println(randomSet)
+//    val listOfString = sentence.split(" ")
+//    println(listOfString)
 
-//    Mape
+/**  Setovi - rijetko se koriste */
+//    val randomSet = setOf("a", "a", "b") // izbacuje duplikate
+//    println(randomSet)
 
-    val userMap = mapOf("oib" to 902139012, "name" to "ivan")
-    println(userMap["oib"])
+/**    Mape */
 
-    printAll<Int>()
+//    val userMap = mapOf("oib" to 902139012, "name" to "ivan")
+//    println(userMap["oib"])
+//
+//    printAll<Int>()
 }
 
-fun <T>printAll(collection: Collection<T>){
-
-    collection.forEach {
-        it:T
-        println(it)
-}
-}
-        fun <T>printObject
+//fun <T>printAll(collection: Collection<T>){
+//
+//    collection.forEach {
+//        it:T
+//        println(it)
+//}
+//}
+//// 10 minuta
+//        fun <T>printObject*/
